28ca0787182016b9173521ce1a3810c53e98751a
Veaceslav Ciornii <89069167+slavablack@users.noreply.github.com>
2022-06-20 13:21:25 +0200

    Veaceslav:header aufgebaut

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
+import { FaUserCircle } from "react-icons/fa";
+import { GiMusicalNotes } from "react-icons/gi";
+import { useEffect } from "react";
 
 function Header() {
   return (
-    <div>header</div>
-  )
+    <div className="header">
+      {" "}
+      <img id="logo" src="Group 1.png" alt="Logo" />
+      <GiMusicalNotes id="music-notes" />
+      <div className="main">
+        <FaUserCircle id="profil-image" />
+        <h3 className="action-header">Sing up</h3>
+        <h3 className="action-header">Log in</h3>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
