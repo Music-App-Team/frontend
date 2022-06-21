cd0353936c2746b84d7af29bf8e9927c2e2c312c
Aisha Noori <aisha.noori2000@gmail.com>
2022-06-21 16:19:49 +0200

    wip

--- src/components/playlistItem/PlaylistItem.js
+++ src/components/playlistItem/PlaylistItem.js
@@ -3,13 +3,7 @@ import React from "react";
 function PlaylistItem() {
   return (
     <div className="test">
-      {" "}
-      <AiFillHome />
-      Home <br />
-      <MdOutlinePlaylistPlay />
-      Play List <br />
-      <FaSistrix />
-      Search Song
+     
     </div>
   );
 }
