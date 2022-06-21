dc08994b0c0617a7d69d1045b2a2f9ef05daa1f2
slavablack <veaceslav.ciornii@magenta.de>
2022-06-21 09:25:02 +0200

    Home page begonnen

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,77 +1,79 @@
 import React from "react";
-import "./footer.scss"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
+import "./footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-    return (
-        <div >
-            <div class="footer pt-4 pb-4 text-center text-md-start position-absolute bottom-0 start-50 translate-middle-x w-75 ">
-                <div class="px-5 ">
-                    <div class="row  ">
-                        <div class="col-md-6 col-lg-3">
-                            <div class="logo ">
-                                <img src="images/musicLogo.png" alt="" />
-                            </div>
-                        </div>
-                        
-                        <div class="col-md-6 col-lg-2 me-5">
-                            <div class="COMPANY">
-                                <h5 class="">COMPANY</h5>
-                                <ul class="list-unstyled lh-lg">
-                                    <li>About</li>
-                                    <li>Jobs</li>
-                                    <li>For the Record</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-2 me-5">
-                            <div class="COMMUNITIES ">
-                                <h5 class="">COMMUNITIES</h5>
-                                <ul class="list-unstyled  lh-lg">
-                                    <li>For Artists</li>
-                                    <li>Developers</li>
-                                    <li>Advertising</li>
-                                    <li>Investors</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-2">
-                            <div class="links">
-                                <h5 class="">Links</h5>
-                                <ul class="list-unstyled lh-lg">
-                                    <li>USEFUL LINKS</li>
-                                    <li>Support</li>
-                                    <li>Web Player</li>
-                                    <li>Free Mobile App</li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                        <div class="col-md-6 col-lg-3 ">
-                            <div class="contact ">
-                                <ul class="d-flex mt-4 position-absolute top-0 end-0  me-5  list-unstyled gap-3">
-                                    <li>
-                                        <a class="d-block icon text-light" href="#">
-                                            <FontAwesomeIcon size="2x" icon={faGithub} /></a>
-                                    </li>
-                                    <li>
-                                        <a class="d-block icon text-light " href="#">
-                                            <FontAwesomeIcon size="2x" icon={faLinkedin} />
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="copyright position-absolute bottom-0 end-0  top-25 start-100 translate-middle text-light">
-                                <span>Germany</span>
-                                <div>&copy;<span>{new Date().getFullYear()}</span></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div class="footer pt-4 pb-4 text-center text-md-start position-absolute bottom-0 start-50 translate-middle-x w-75 ">
+        <div class="px-5 ">
+          <div class="row  ">
+            <div class="col-md-6 col-lg-3">
+              <div class="logo ">
+                <img src="images/musicLogo.png" alt="" />
+              </div>
             </div>
+
+            <div class="col-md-6 col-lg-2 me-5">
+              <div class="COMPANY">
+                <h5 class="">COMPANY</h5>
+                <ul class="list-unstyled lh-lg">
+                  <li>About</li>
+                  <li>Jobs</li>
+                  <li>For the Record</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-2 me-5">
+              <div class="COMMUNITIES ">
+                <h5 class="">COMMUNITIES</h5>
+                <ul class="list-unstyled  lh-lg">
+                  <li>For Artists</li>
+                  <li>Developers</li>
+                  <li>Advertising</li>
+                  <li>Investors</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-2">
+              <div class="links">
+                <h5 class="">Links</h5>
+                <ul class="list-unstyled lh-lg">
+                  <li>USEFUL LINKS</li>
+                  <li>Support</li>
+                  <li>Web Player</li>
+                  <li>Free Mobile App</li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3 ">
+              <div class="contact ">
+                <ul class="d-flex mt-4 position-absolute top-0 end-0  me-5  list-unstyled gap-3">
+                  <li>
+                    <a class="d-block icon text-light" href="#">
+                      <FontAwesomeIcon size="2x" icon={faGithub} />
+                    </a>
+                  </li>
+                  <li>
+                    <a class="d-block icon text-light " href="#">
+                      <FontAwesomeIcon size="2x" icon={faLinkedin} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="copyright position-absolute bottom-0 end-0  top-25 start-100 translate-middle text-light">
+                <span>Germany</span>
+                <div>
+                  &copy;<span>{new Date().getFullYear()}</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default Footer;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { MdOutlinePlaylistPlay } from "react-icons/md";
+import { FaSistrix } from "react-icons/fa";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <AiFillHome />
+      Home <br />
+      <MdOutlinePlaylistPlay />
+      Play List <br />
+      <FaSistrix />
+      Search Song
+    </div>
+  );
 }
 
-export default Home
+export default Home;
