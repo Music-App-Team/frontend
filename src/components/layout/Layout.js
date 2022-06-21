343b19f272daf5c273c16a24c8b716b24ae0b797
slavablack <veaceslav.ciornii@magenta.de>
2022-06-21 16:06:55 +0200

    playlist item done

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,12 +6,14 @@ import Search from "../../pages/search/Search";
 import Playlist from "../../pages/playlists/Playlist";
 import PlaylistDetail from "../../pages/playlistDetail/PlaylistDetail";
 import Footer from "../footer/Footer";
+import PlaylistItem from "../playlistItem/PlaylistItem";
 import "bootstrap/dist/css/bootstrap.css";
 
 function layout() {
   return (
     <div>
       <Header />
+      <PlaylistItem />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/search" element={<Search />}></Route>
