46045f9292637f4964f9ee8775527e226f9d154c
slavablack <veaceslav.ciornii@magenta.de>
2022-06-23 09:56:23 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import Layout from './layout/Layout'
-import {Routes,Route} from 'react-router-dom'
-import Login from '../pages/login/Login'
+import Layout from "./layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import Landing from "../pages/landing/Landing";
-
+import Search from "../pages/search/Search";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/search" element={<Search />}></Route>
         <Route path="/login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/landing" element={<Landing />}></Route>
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../header/Header";
 import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/home/Home";
-import Search from "../../pages/search/Search";
 import Playlist from "../../pages/playlists/Playlist";
 import PlaylistDetail from "../../pages/playlistDetail/PlaylistDetail";
 import Footer from "../footer/Footer";
@@ -16,11 +15,10 @@ function layout() {
       <PlaylistItem />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/search" element={<Search />}></Route>
         <Route path="/playlists" element={<Playlist />}></Route>
         <Route path="/playlistDetail/:id" element={<PlaylistDetail />}></Route>
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
