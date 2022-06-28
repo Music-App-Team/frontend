8540e540e3dca7eb9486abaa940434d671e622cc
Mazen <mazen.taha88@gmail.com>
2022-06-28 14:48:25 +0200

    wip

--- src/pages/playlists/Playlist.js
+++ src/pages/playlists/Playlist.js
@@ -2,47 +2,95 @@ import React from "react";
 import "./playList.scss";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
+import { MdOutlineStarOutline } from "react-icons/md";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+
 
 
 
 function Playlist() {
   return (
-    <div class="container bg-black">
-      <Container>
-        <h2 class="playlist-h2"> Playlist</h2>
-        <Table striped bordered hover size="sm">
+    <div className="container-fluid">
+      <Container className="container-playlist">
+        <div className="header-playlist">
+          <h1 className="playlist-h1">Play List</h1>
+          <button className="button-create">
+            Create
+          </button>
+        </div>
+        <Table className="playlist-table">
           <thead>
             <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Name</th>
+              <th>Author</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td>Happy songs</td>
+              <td>Saman</td>
+              <td>
+                <div className="container-action">
+                  {" "}
+                  <button className="button-playlist">View</button>
+                  <a className="like" href="url">
+                    <AiOutlineLike />
+                  </a>
+                  <a className="dislike" href="url">
+                    <AiOutlineDislike />
+                  </a>
+                  <a className="star" href="url">
+                    <MdOutlineStarOutline />
+                  </a>
+                </div>
+              </td>
             </tr>
             <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+              <td>Light songs</td>
+              <td>John</td>
+              <td>
+                <div className="container-action">
+                  {" "}
+                  <button className="button-playlist">View</button>
+                  <a className="like" href="url">
+                    <AiOutlineLike />
+                  </a>
+                  <a className="dislike" href="url">
+                    <AiOutlineDislike />
+                  </a>
+                  <a className="star" href="url">
+                    <MdOutlineStarOutline />
+                  </a>
+                </div>
+              </td>
             </tr>
             <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
+              <td>Sad songs</td>
+              <td>Sara</td>
+              <td>
+                <div className="container-action">
+                  {" "}
+                  <button className="button-playlist">View</button>
+                  <a className="like" href="url">
+                    <AiOutlineLike />
+                  </a>
+                  <a className="dislike" href="url">
+                    <AiOutlineDislike />
+                  </a>
+                  <a className="star" href="url">
+                    <MdOutlineStarOutline />
+                  </a>
+                </div>
+              </td>
             </tr>
           </tbody>
         </Table>
       </Container>
     </div>
   );
-
 }
 
 export default Playlist;
+
