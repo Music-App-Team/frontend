760671f291f94778fdeb9db8eb88ba17b70a9651
Aisha Noori <aisha.noori2000@gmail.com>
2022-07-05 14:52:56 +0200

    added modal1.js and createPlaylistModal.js

--- src/pages/playlists/Playlist.js
+++ src/pages/playlists/Playlist.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./playList.scss";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import { MdOutlineStarOutline } from "react-icons/md";
-import { AiOutlineLike } from "react-icons/ai";
-import { AiOutlineDislike } from "react-icons/ai";
-
-
-
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
+import CreatePlaylistModal from "./createPlaylistModal";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function Playlist() {
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [playlists, setPlaylists] = useState([]);
+
+  function handleCreatePlaylist() {
+    setOpenCreateModal(true);
+  }
+
+  useEffect(() => {
+    getPlayList();
+  }, []);
+
+  function getPlayList() {
+    axios
+      .get("http://localhost:3010/playlist/getAllPlaylists")
+      .then((res) => setPlaylists(res.data))
+      .catch((err) => toast.error(err.response?.data?.message || err.message));
+  }
+
   return (
     <div className="container-fluid">
       <Container className="container-playlist">
         <div className="header-playlist">
           <h1 className="playlist-h1">Play List</h1>
-          <button className="button-create">
+          <button className="button-create" onClick={handleCreatePlaylist}>
             Create
           </button>
         </div>
@@ -28,69 +45,34 @@ function Playlist() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Happy songs</td>
-              <td>Saman</td>
-              <td>
-                <div className="container-action">
-                  {" "}
-                  <button className="button-playlist">View</button>
-                  <a className="like" href="url">
-                    <AiOutlineLike />
-                  </a>
-                  <a className="dislike" href="url">
-                    <AiOutlineDislike />
-                  </a>
-                  <a className="star" href="url">
-                    <MdOutlineStarOutline />
-                  </a>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Light songs</td>
-              <td>John</td>
-              <td>
-                <div className="container-action">
-                  {" "}
-                  <button className="button-playlist">View</button>
-                  <a className="like" href="url">
-                    <AiOutlineLike />
-                  </a>
-                  <a className="dislike" href="url">
-                    <AiOutlineDislike />
-                  </a>
-                  <a className="star" href="url">
-                    <MdOutlineStarOutline />
-                  </a>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Sad songs</td>
-              <td>Sara</td>
-              <td>
-                <div className="container-action">
-                  {" "}
-                  <button className="button-playlist">View</button>
-                  <a className="like" href="url">
-                    <AiOutlineLike />
-                  </a>
-                  <a className="dislike" href="url">
-                    <AiOutlineDislike />
-                  </a>
-                  <a className="star" href="url">
-                    <MdOutlineStarOutline />
-                  </a>
-                </div>
-              </td>
-            </tr>
+            {playlists.map((item) => (
+              <tr>
+                <td>{item.title}</td>
+                <td>Eli</td>
+                <td>
+                  <div className="container-action">
+                    {" "}
+                    <button className="button-playlist">View</button>
+                    <a className="like" href="url">
+                      <AiOutlineLike />
+                    </a>
+                    <a className="dislike" href="url">
+                      <AiOutlineDislike />
+                    </a>
+                    <a className="star" href="url">
+                      <MdOutlineStarOutline />
+                    </a>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Container>
+      <CreatePlaylistModal open={openCreateModal} onUpdate={getPlayList} onClose={() => setOpenCreateModal(false)}
+      />
     </div>
   );
 }
 
 export default Playlist;
-
