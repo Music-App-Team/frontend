8f271dfdc39a447bf610105d07e6954c82977727
Aisha Noori <aisha.noori2000@gmail.com>
2022-07-07 12:33:58 +0200

    done addfavorit

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./dashboard.scss";
 import { CgTrashEmpty } from "react-icons/cg";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
-
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 function Dashboard() {
+  const [playlists, setPlaylists] = useState([]);
+
+  function getData() {
+    axios
+      .get("/user/getFavoritePlaylists")
+      .then((res) => {
+        setPlaylists(res.data);
+      })
+      .catch((err) => toast.error(err.response?.data?.message || err.message));
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function handleRemovePlaylistFromFavorite(id) {
+    axios
+      .delete("/user/removePlaylistFromFavorite/" + id)
+      .then((res) => {
+        toast.success("success");
+        getData();
+      })
+      .catch((err) => toast.error(err.response?.data?.message || err.message));
+  }
+
   return (
     <div className="container-fluid">
-      <Container className="container-playlist">
-        <h1 className="playlist-h1">Home</h1>
+      <Container className="container-dashboard">
+        <h1 className="dashboard-h1">Home</h1>
         <Table className="playlist-table">
           <thead>
             <tr>
@@ -18,45 +44,24 @@ function Dashboard() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Happy songs</td>
-              <td>Saman</td>
-              <td>
-                <div className="container-action">
-                  {" "}
-                  <button className="button-playlist">Open</button>
-                  <a href="url">
-                    <CgTrashEmpty className="trash" />
-                  </a>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Light songs</td>
-              <td>John</td>
-              <td>
-                <div className="container-action">
-                  {" "}
-                  <button className="button-playlist">Open</button>
-                  <a href="url">
-                    <CgTrashEmpty className="trash" />
-                  </a>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Sad songs</td>
-              <td>Sara</td>
-              <td>
-                <div className="container-action">
-                  {" "}
-                  <button className="button-playlist">Open</button>
-                  <a href="url">
-                    <CgTrashEmpty className="trash" />
-                  </a>
-                </div>
-              </td>
-            </tr>
+            {playlists.map((item) => (
+              <tr>
+                <td>{item.title}</td>
+                <td>{item.user?.firstName}</td>
+                <td>
+                  <div className="container-action">
+                    <Link to={`/app/playlist/${item._id}`}>
+                        <button className="button-playlist">Open</button>
+                    </Link>
+                    <span
+                      onClick={() => handleRemovePlaylistFromFavorite(item._id)}
+                    >
+                      <CgTrashEmpty className="trash" />
+                    </span>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Container>
@@ -65,5 +70,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-
