cfba34ee96c3fb10fdc2741c68a084b1265ad085
jaber-1988 <jaber1988.1988@gmail.com>
2022-07-13 10:56:33 +0200

    wip

--- src/pages/playlistDetail/PlaylistDetail.js
+++ src/pages/playlistDetail/PlaylistDetail.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom";
 import Modal from '../../components/modal/Modal'
 import { useState } from 'react';
 import "./playListDetail.scss";
-
+import axios from 'axios';
+import { toast } from "react-toastify";
+import { useParams} from "react-router-dom";
 
 import { BsPlay } from 'react-icons/bs';
 import { AiOutlineCloudDownload, AiFillPlayCircle, AiFillMinusCircle, AiOutlineComment } from 'react-icons/ai';
@@ -12,27 +14,52 @@ import { RiDeleteBin5Line } from 'react-icons/ri';
 
 
 
+
+
 function PlaylistDetail() {
   const [isOpen, setIsOpen] = useState(false)
+
+  const [playlistName,setPlaylistName]=useState(null)
+
+  const params = useParams();
+
+ useEffect(()=>{
+function playlistname(){ 
+    
+     axios.get(`http://localhost:3010/playlist/getPlaylistById/${params.id}`)
+      .then((res) => setPlaylistName(res.data))
+      .catch((err) => toast.error(err.response?.data?.message || err.message));
+} 
+ if (params?.id){
+  playlistname()
+ }
+ },[params.id]);
 
   return (
     <div>
       <div class="container-playlistdetail">
-        <div class="playlistaction ">
+      
+       <div class="playlistaction ">
           <div> <img src="/images/playlistdetail.png" className="rounded float-start " alt="" /></div>
+          
           <div class="playall row ">
-            <p>Light Musics</p>
-            <p>5 songs <Link className="link" to="/app/comment"><AiOutlineComment /></Link></p>
+             {playlistName?<p>{playlistName.title}</p>:undefined}
+                
+          
+           
+            <p> {playlistName?.songs?.length}songs <Link className="link" to="/app/comment"><AiOutlineComment /></Link></p>
             <button className="play">Play All <BsPlay /></button>
           </div>
           <div class="download"><button>Download <AiOutlineCloudDownload /></button></div>
           <div class="rename"><button>Rename <MdDriveFileRenameOutline /></button></div>
           <div class="delete"><button>Delete <RiDeleteBin5Line /></button></div>
-          <div class="add-song"><button onClick={() => setIsOpen(true)}>Add Song <MdAddBox /></button>
+          <div class="add-song"><button onClick={() => setIsOpen(true)}>Add Song <MdAddBox /></button> 
+           
           <Modal open={isOpen} onClose={() => setIsOpen(false)}>
         </Modal>
           </div>
         </div>
+        
         <hr  className="hr"/>
         <div className="list-songs">
           <ul class="list-group list-group-horizontal ms-4 mt-5 ">
