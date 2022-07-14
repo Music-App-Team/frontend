40cd3b53c45e5c9deed065be1ab663208f9e9d52
Eli <89070187+aisha2000-hub@users.noreply.github.com>
2022-07-14 10:34:51 +0200

    Eli: done play song
    * added mixin and sidebar and dashboard
    
    * wip
    
    * add anything and delete
    
    * wip
    
    * wip
    
    * login and signup done
    
    * added modal1.js and createPlaylistModal.js
    
    * done addfavorit
    
    * wip
    
    * done profileImage
    
    * wip
    
    * wip
    
    * done addSong
    
    * done play song

--- src/components/musicItem/MusicItem.js
+++ src/components/musicItem/MusicItem.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import { AiFillPlayCircle, AiFillMinusCircle } from "react-icons/ai";
+import React, { useEffect,useState, useRef } from 'react'
+import { AiFillPlayCircle, AiFillMinusCircle, AiFillPauseCircle } from "react-icons/ai";
 
-function MusicItem({music}) {
+function MusicItem({ music }) {
+  const audio = useRef(new Audio(music.link));
+  const [playing ,setPlaying] = useState(false);
+
+
+  useEffect(() => {
+    audio.current.onplaying = () => {
+      setPlaying(true)
+    }
+    audio.current.onpause = () => {
+      setPlaying(false)
+    }
+
+  })
+
+  const handlePlay = () => {
+    if(playing) audio.current.pause()
+    else audio.current.play()
+  }
+
   return (
     <div>
       <div class="list-group list-group-horizontal ms-4 mt-5 ">
         <span class="list-group-item">{music.name}</span>
         <span class="list-group-item">
-          <AiFillPlayCircle />
+          {playing ? (
+            <AiFillPauseCircle onClick={handlePlay} />
+          ) : (
+            <AiFillPlayCircle onClick={handlePlay} />
+          )}
           <AiFillMinusCircle />
-        </span>
+        </span> 
         <span class="list-group-item">{music.artist}</span>
         <span class="list-group-item">{music.album}</span>
         <span class="list-group-item">{music.lang}</span>
