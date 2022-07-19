bc0fb4d80c7fd20d4d02dc12a8236edb29e74750
Jaber Abdullah <85929642+jaber-1988@users.noreply.github.com>
2022-07-19 15:10:58 +0200

    Jaber: comment-fix-reload
    * delete line from playlist.js
    
    * wip

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -5,47 +5,40 @@ import { toast } from "react-toastify";
 import Moment from 'react-moment';
 
 
+
+
+
 export const Comment = () => {
 
-    const [comments, setComments] = useState("")
+    const [comments, setComments] = useState({ text: "" })
 
     const [getComments, setGetComments] = useState([]);
 
 
-
-    // useEffect((comments) => {
     const handleSubmit = () => {
         axios.post("http://localhost:3010/playlist/addComment", { comments })
             .then(res => {
                 toast.success("comment added successfully")
-                window.location.reload()
             }).catch(err => toast.error(err.response?.data?.message || err.message))
     }
-    // handleSubmit()
-// },[]);
+
+
     const deletecomment = (id) => {
         axios.delete(`http://localhost:3010/playlist/deleteComment/${id}`)
             .then(res => {
                 toast.success("comment deleted successfully")
             }).catch(err => toast.error(err.response?.data?.message || err.message))
-            getComment()
     }
 
     useEffect(() => {
-        getComment()
-
-    }, [comments]);
-
-
-    
         function getComment() {
             axios
                 .get("http://localhost:3010/playlist/getAllComments")
                 .then((res) => setGetComments(res.data))
                 .catch((err) => toast.error(err.response?.data?.message || err.message));
         }
-
-
+        getComment()
+    }, [getComments]);
 
     return (
         <div class="container-comment ">
@@ -64,7 +57,7 @@ export const Comment = () => {
                                                 </div>
                                                 <div class="mt-2">
                                                     <p class="mx-0">{item.comments[0].text}</p>
-                                                    <div class="mt-2 text-right"><button class="btn-delete  float-end btn-primary btn-sm shadow-none" type="button" onClick={()=>deletecomment(item._id)}>Delete</button></div>
+                                                    <div class="mt-2 text-right"><button class="btn-delete  float-end btn-primary btn-sm shadow-none" type="button" onClick={() => deletecomment(item._id)}>Delete</button></div>
                                                 </div>
                                             </div><hr className="hr" />
                                         </div>
@@ -75,7 +68,7 @@ export const Comment = () => {
                     }) : undefined}
                     <div class="d-flex flex-column p-2">
                         <div class="d-flex flex-row align-items-start"><textarea placeholder="Review" class="form-control ml-1 shadow-none textarea" value={comments.text} onChange={(e) => setComments({ text: e.target.value })}></textarea></div>
-                        <div class="mt-2 text-right"><button class="btn  float-end btn-primary btn-sm shadow-none" type="button" onClick={handleSubmit}   >Submit</button></div>
+                        <div class="mt-2 text-right"><button class="btn  float-end btn-primary btn-sm shadow-none" type="button" onClick={() => { handleSubmit(); setComments({ text: "" }) }}  >Submit</button></div>
                     </div>
                 </div>
             </div>
