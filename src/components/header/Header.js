14b4bcc1627ae774035ee50b99062498b1d9e1c3
Eli <89070187+aisha2000-hub@users.noreply.github.com>
2022-08-07 11:41:51 +0200

    Eli: fixed responsive
    * einige scss änderung
    
    * added gif in musicItem.js
    
    * test
    
    * test1
    
    * added playerCommponent and responsive
    
    * added faBar Icon in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Header.scss";
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle, FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useUserInfoContext } from "../../context/UserInfoContext";
 
-function Header() {
+function Header({ toggleSidebar }) {
   const { signoutContext, isAuthenticated, userInfo } = useUserInfoContext();
 
   function handleSignout() {
@@ -14,15 +14,20 @@ function Header() {
 
   return (
     <div className="header">
-       <Link to={"/"}><img id="logo" src="/images/Logo.png" alt="Logo" /></Link>
-
+      <FaBars className="header__menu" onClick={toggleSidebar} />
+      <Link to={"/"} className="header__logo">
+        <img id="logo" src="/images/Logo.png" alt="Logo" />
+      </Link>
 
       <div className="main">
         <Link to="/app/profile">
           {!userInfo.image ? (
             <FaUserCircle id="profil-image" />
           ) : (
-            <div className="profileImage"  style={{backgroundImage: `url(${userInfo.image})`}} />
+            <div
+              className="profileImage"
+              style={{ backgroundImage: `url(${userInfo.image})` }}
+            />
           )}
         </Link>
         {isAuthenticated ? (
